1263e77ec4aa50141226ce19bfcf3d2a1d198e71
Valldu <gerard.valldosera@institutvidreres.ca>
2023-03-29 09:57:22 +0200

    Fet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FitxersAngular';
+  // @ts-ignore
+   constructor(private http:HttpClient) {
+
+     //EX1
+     this.http.post<any>("http://localhost:3000/api/ex1", {}).subscribe();
+
+     //EX2
+     this.http.post<any>("http://localhost:3000/api/ex2", {}).subscribe();
+
+     //EX3
+     this.http.post<any>("http://localhost:3000/api/ex3", {}).subscribe();
+
+     //EX4
+     this.http.post<any>("http://localhost:3000/api/ex4", {}).subscribe();
+    }
 }
---
+++ src/app/fitxers/fitxers.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fitxers',
+  templateUrl: './fitxers.component.html',
+  styleUrls: ['./fitxers.component.css']
+})
+export class FitxersComponent {
+
+}
